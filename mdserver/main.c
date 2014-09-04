0b20fef994faab6f41cb2764cc5a85363ec4f4a8
Rui Xiao <rui.xiao@suanzi.cn>
2014-09-04 20:55:49 +0800

    minor

--- mdserver/main.c
+++ mdserver/main.c
@@ -12,6 +12,49 @@
 #include "mdcallback.h"
 #include "vbmal.h"
 
+void readinfo(char *filename, char **logfilepath, char **server, char **BrokerID, char **UserID, char **pd, char *(*InstrumentIDs)[], int *InstrumentNum) {
+	FILE *fp = sfopen(filename, "r");
+	char line[1000];
+	int i = 0;
+	int j = 0;
+	while(fgets(line, 1000, fp)) {
+		switch(i++) {
+			case 0:
+				*logfilepath = smalloc(strlen(line));
+				memcpy(*logfilepath, line, strlen(line));
+				(*logfilepath)[strlen(line)-1] = '\0';
+				break;
+			case 1:
+				*server = smalloc(strlen(line));
+				memcpy(*server, line, strlen(line));
+				(*server)[strlen(line)-1] = '\0';
+				break;
+			case 2:
+				*BrokerID= smalloc(strlen(line));
+				memcpy(*BrokerID, line, strlen(line));
+				(*BrokerID)[strlen(line)-1] = '\0';
+				break;
+			case 3:
+				*UserID= smalloc(strlen(line));
+				memcpy(*UserID, line, strlen(line));
+				(*UserID)[strlen(line)-1] = '\0';
+				break;
+			case 4:
+				*pd = smalloc(strlen(line));
+				memcpy(*pd, line, strlen(line));
+				(*pd)[strlen(line)-1] = '\0';
+				break;
+			default:
+				(*InstrumentIDs)[j] = smalloc(strlen(line));
+				memcpy((*InstrumentIDs)[j], line, strlen(line));
+				(*InstrumentIDs)[j][strlen(line)-1] = '\0';
+				j++;
+				break;
+		}
+	}
+	*InstrumentNum = j;
+}
+
 struct MongoIM {
 	void *md;
 	mongoc_client_t *client;
@@ -100,49 +143,6 @@ void *ProcessDMD(void *mim_p) {
 			insert_mongodb(client, collection, pDepthMarketData, 0.1, 0.1);
 		}
 	}
-}
-
-void readinfo(char *filename, char **logfilepath, char **server, char **BrokerID, char **UserID, char **pd, char *(*InstrumentIDs)[], int *InstrumentNum) {
-	FILE *fp = sfopen(filename, "r");
-	char line[1000];
-	int i = 0;
-	int j = 0;
-	while(fgets(line, 1000, fp)) {
-		switch(i++) {
-			case 0:
-				*logfilepath = smalloc(strlen(line));
-				memcpy(*logfilepath, line, strlen(line));
-				(*logfilepath)[strlen(line)-1] = '\0';
-				break;
-			case 1:
-				*server = smalloc(strlen(line));
-				memcpy(*server, line, strlen(line));
-				(*server)[strlen(line)-1] = '\0';
-				break;
-			case 2:
-				*BrokerID= smalloc(strlen(line));
-				memcpy(*BrokerID, line, strlen(line));
-				(*BrokerID)[strlen(line)-1] = '\0';
-				break;
-			case 3:
-				*UserID= smalloc(strlen(line));
-				memcpy(*UserID, line, strlen(line));
-				(*UserID)[strlen(line)-1] = '\0';
-				break;
-			case 4:
-				*pd = smalloc(strlen(line));
-				memcpy(*pd, line, strlen(line));
-				(*pd)[strlen(line)-1] = '\0';
-				break;
-			default:
-				(*InstrumentIDs)[j] = smalloc(strlen(line));
-				memcpy((*InstrumentIDs)[j], line, strlen(line));
-				(*InstrumentIDs)[j][strlen(line)-1] = '\0';
-				j++;
-				break;
-		}
-	}
-	*InstrumentNum = j;
 }
 
 
